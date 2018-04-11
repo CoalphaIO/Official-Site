b756f9f08953921b0acfca45f3da8fc7f086a07e
jieouy <jieouy@126.com>
2018-04-11 18:16:31 +0800

    同步提交代码。添加eth的策略内容展示

--- js/strat_crypto.js
+++ js/strat_crypto.js
@@ -4,18 +4,18 @@ $(document).ready(function() {
         data: {
             global: {},
             stratReports: [{
-                'strat-name': 'CoAlpha Strategy',
+                'strat-name': 'CoAlpha Strategy(based on BTC) ',
                 'industry-name': 'CoAlpha Inc.',
-                'description': 'The CoAlpha MOM30 index product is a cryptocurrency investment product that adopts index strategy. This index strategy is a simple quantitative strategy that dynamically adjusts the portfolio weights of 30 cryptocurrency based on market capital and momentum. It can achieve a higher return to risk ratio  than holding a single digital currency such as BTC, due to investment diversification and dynamic allocation.  For 2017, this strategy has a return of 17070% with Sharpe Ratio 4.82 and Sortino Ratio 7.12, versus BTC\'s return of 1384% with Sharpe Ratio 3.11 and Sortino Ratio 4.55. The advantages for this strategy are: better diversification, easy to understand, easy to implement the trade execution, and large capital capacity. So it is suitable for large scale asset allocation and less sophisticated investors.',
+                'description': 'The CoAlpha MOM30 index product is a cryptocurrency investment product that adopts index strategy. This index strategy is a simple quantitative strategy that dynamically adjusts the portfolio weights of 30 cryptocurrency based on market capital and momentum. It can achieve a higher return to risk ratio  than holding a single digital currency such as BTC, due to investment diversification and dynamic allocation.  In last year, this strategy has a return of 5418% with Sharpe Ratio 4.82 and Sortino Ratio 7.12, versus ETH\'s return of 129% with Sharpe Ratio 0.74 and Sortino Ratio 1.04. The advantages for this strategy are: better diversification, easy to understand, easy to implement the trade execution, and large capital capacity. So it is suitable for large scale asset allocation and less sophisticated investors.',
                 'stock-list-list': {
                     "top": [],
                     "bottom": []
                 },
                 'imgFluid': 'Strategy-img-fluid-1'
             }, {
-                'strat-name': 'CoAlpha Strategy',
+                'strat-name': 'CoAlpha Strategy(based on ETH)',
                 'industry-name': 'CoAlpha Inc.',
-                'description': 'The CoAlpha MOM30 index product is a cryptocurrency investment product that adopts index strategy. This index strategy is a simple quantitative strategy that dynamically adjusts the portfolio weights of 30 cryptocurrency based on market capital and momentum. It can achieve a higher return to risk ratio  than holding a single digital currency such as BTC, due to investment diversification and dynamic allocation.  For 2017, this strategy has a return of 17070% with Sharpe Ratio 4.82 and Sortino Ratio 7.12, versus BTC\'s return of 1384% with Sharpe Ratio 3.11 and Sortino Ratio 4.55. The advantages for this strategy are: better diversification, easy to understand, easy to implement the trade execution, and large capital capacity. So it is suitable for large scale asset allocation and less sophisticated investors.',
+                'description': 'This product is based on ETH. In last year, this strategy has a return of 2674% with Sharpe Ratio 3.54 and Sortino Ratio 4.55, versus BTC\'s return of 78% with Sharpe Ratio 0.35 and Sortino Ratio 0.43.',
                 'stock-list-list': {
                     "top": [],
                     "bottom": []
@@ -41,68 +41,11 @@ $(document).ready(function() {
         }
 
     });
-    var btcChart = echarts.init(document.getElementById('Strategy-img-fluid-1'));
-    $.get('js/crypto_nav.json', function(data) {
-        btcChart.setOption(option = {
+    var usdChart = echarts.init(document.getElementById('Strategy-img-fluid-1'));
+    $.get('js/crypto_btc.json', function(data) {
+        usdChart.setOption(option = {
             title: {
-                subtext: 'MOM30Index:sharpe=4.82;sortino=7.12\nBTC:sharpe=3.11;sortino=4.55'
-            },
-            legend: {
-
-                data: ['MOM30Index', 'BTC'],
-                top: 40
-            },
-            tooltip: {
-                trigger: 'axis'
-            },
-            xAxis: {
-                axisLabel: {
-                    formatter: function(category) {
-                        return category.substring(0, 6);
-                    }
-                },
-                data: data.map(function(item) {
-                    return item[0];
-                })
-            },
-            yAxis: {
-                title: 'NAV',
-                splitLine: {
-                    show: false
-                }
-            },
-            toolbox: {
-                left: 'center',
-                feature: {
-                    dataZoom: {
-                        yAxisIndex: 'none'
-                    },
-                    restore: {},
-                    saveAsImage: {}
-                }
-            },
-            series: [{
-                    name: 'MOM30Index',
-                    type: 'line',
-                    data: data.map(function(item) {
-                        return item[1];
-                    }),
-                },
-                {
-                    name: 'BTC',
-                    type: 'line',
-                    data: data.map(function(item) {
-                        return item[2];
-                    }),
-                }
-            ]
-        });
-    });
-    var ethChart = echarts.init(document.getElementById('Strategy-img-fluid-2'));
-    $.get('js/crypto_nav.json', function(data) {
-        ethChart.setOption(option = {
-            title: {
-                subtext: 'MOM30Index:sharpe=4.82;sortino=7.12\nBTC:sharpe=3.11;sortino=4.55'
+                subtext: 'MOM30Index:sharpe=4.95;sortino=5.87\nETH:sharpe=0.74;sortino=1.04'
             },
             legend: {
 
@@ -115,7 +58,7 @@ $(document).ready(function() {
             xAxis: {
                 axisLabel: {
                     formatter: function(category) {
-                        return category.substring(0, 6);
+                        return category.substring(4, 8);
                     }
                 },
                 data: data.map(function(item) {
@@ -147,6 +90,63 @@ $(document).ready(function() {
                 },
                 {
                     name: 'ETH',
+                    type: 'line',
+                    data: data.map(function(item) {
+                        return item[2];
+                    }),
+                }
+            ]
+        });
+    });
+    var ethChart = echarts.init(document.getElementById('Strategy-img-fluid-2'));
+    $.get('js/crypto_eth.json', function(data) {
+        ethChart.setOption(option = {
+            title: {
+                subtext: 'MOM30Index:sharpe=3.54;sortino=4.55\nBTC:sharpe=0.35;sortino=0.43'
+            },
+            legend: {
+
+                data: ['MOM30Index', 'BTC'],
+                top: 40
+            },
+            tooltip: {
+                trigger: 'axis'
+            },
+            xAxis: {
+                axisLabel: {
+                    formatter: function(category) {
+                        return category.substring(4, 8);
+                    }
+                },
+                data: data.map(function(item) {
+                    return item[0];
+                })
+            },
+            yAxis: {
+                title: 'NAV',
+                splitLine: {
+                    show: false
+                }
+            },
+            toolbox: {
+                left: 'center',
+                feature: {
+                    dataZoom: {
+                        yAxisIndex: 'none'
+                    },
+                    restore: {},
+                    saveAsImage: {}
+                }
+            },
+            series: [{
+                    name: 'MOM30Index',
+                    type: 'line',
+                    data: data.map(function(item) {
+                        return item[1];
+                    }),
+                },
+                {
+                    name: 'BTC',
                     type: 'line',
                     data: data.map(function(item) {
                         return item[2];
